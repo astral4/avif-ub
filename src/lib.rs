5f1e0d838658f6c5b4ca7120c345e59942c8dbba
astral4 <88992929+astral4@users.noreply.github.com>
2024-01-17 23:49:45 -0500

    clean up lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,6 @@
-//! ```rust
-//! use ravif::*;
-//! # fn doit(pixels: &[RGBA8], width: usize, height: usize) -> Result<(), Error> {
-//! let res = Encoder::new()
-//!     .with_quality(70.)
-//!     .with_speed(4)
-//!     .encode_rgba(Img::new(pixels, width, height))?;
-//! std::fs::write("hello.avif", res.avif_file);
-//! # Ok(()) }
-
 mod av1encoder;
-
 mod error;
+
 pub use av1encoder::EncodedImage;
 pub use av1encoder::Encoder;
 pub use error::Error;
-#[doc(inline)]
-pub use rav1e::prelude::MatrixCoefficients;
-
-#[doc(no_inline)]
-pub use imgref::Img;
-#[doc(no_inline)]
-pub use rgb::{RGB8, RGBA8};
